14aa87123baafc351815ecabb0eced66193d90ec
Matt Schreiber <tomeon@dogea.red>
2024-07-14 09:06:00 -0400

    doc(treewide): s/symlink/bind-mount/
    as asd now uses bind-mounts rather than symlinks for exposing the
    tmpfs-es that it manages.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Anything-sync-daemon (asd) is a tiny pseudo-daemon designed to manage user
 defined dirs in tmpfs and to periodically sync back to the physical disc
-(HDD/SSD). This is accomplished via a symlinking step and an innovative use of
-rsync to maintain back-up and synchronization between the two. One of the major
-design goals of asd is a completely transparent user experience.
+(HDD/SSD). This is accomplished via a bind-mounting step and an innovative use
+of rsync to maintain back-up and synchronization between the two. One of the
+major design goals of asd is a completely transparent user experience.
 
 ## Documentation
 
--- nix/packages.nix
+++ nix/packages.nix
@@ -68,11 +68,11 @@
             description = "tmpfs -> physical disk data synchronization pseudo-daemon";
             longDescription = ''
               Anything-sync-daemon (asd) is a tiny pseudo-daemon designed to
-              manage user defined dirs in tmpfs and to periodically sync back to
-              the physical disc (HDD/SSD). This is accomplished via a symlinking
-              step and an innovative use of rsync to maintain back-up and
-              synchronization between the two. One of the major design goals of
-              asd is a completely transparent user experience.
+              manage user defined dirs in tmpfs and to periodically sync back
+              to the physical disc (HDD/SSD). This is accomplished via a
+              bind-mounting step and an innovative use of rsync to maintain
+              back-up and synchronization between the two. One of the major
+              design goals of asd is a completely transparent user experience.
             '';
             downloadPage = "https://github.com/graysky2/profile-sync-daemon/releases";
             license = lib.licenses.mit;
